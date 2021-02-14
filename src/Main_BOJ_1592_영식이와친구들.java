15b60933bb19170fd7a4f91ecb05824616e1e9d2
ByungBok-Jeon <ttlg789@gmail.com>
2021-02-14 19:20:26 +0900

    2021.02.14

---
+++ src/Main_BOJ_1592_영식이와친구들.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Main_BOJ_1592_영식이와친구들 {
+	static int N;
+	static int M;
+	static int L;
+	static int[] map;
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		N = scan.nextInt();
+		M = scan.nextInt();
+		L = scan.nextInt();
+		map = new int[N];
+
+		map[0] = 1;
+		int tmp = 0;
+		while (true) {
+
+			if (map[tmp] % 2 == 1) {
+				tmp = (tmp + L) % N;
+				map[tmp]++;
+
+			} else if (map[tmp] % 2 == 0) {
+				if (tmp - L < 0)
+					tmp = tmp - L + N;
+				else
+					tmp = tmp - L;
+				map[tmp]++;
+			}
+			if (map[tmp] == M)
+				break;
+		}
+
+		int cnt = 0;
+		for (int i = 0; i < N; i++) {
+			cnt += map[i];
+		}
+		System.out.println(cnt - 1);
+
+	}
+
+}
---
+++ src/Solution_D3_11315_오목판정.java
@@ -0,0 +1,106 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Solution_D3_11315_오목판정 {
+	static int T;
+	static int N;
+	static char[][] map;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		T = Integer.parseInt(br.readLine());
+		for (int t = 1; t <= T; t++) {
+			N = Integer.parseInt(br.readLine());
+			map = new char[N][N];
+			for (int i = 0; i < N; i++) {
+				String s = br.readLine();
+				for (int j = 0; j < N; j++) {
+					map[i][j] = s.charAt(j);
+				}
+			}
+			bw.write("#" + t + " " + five());
+			bw.newLine();
+			bw.flush();
+		}
+		bw.close();
+	}
+
+	static String five() {
+		if (rCheck() || cCheck() || diaCheck())
+			return "YES";
+		else
+			return "NO";
+	}
+
+	static boolean rCheck() {
+		for (int i = 0; i < N; i++) {
+			String s = "";
+			for (int j = 0; j < N; j++) {
+				s += map[i][j];
+			}
+			if (s.contains("ooooo"))
+				return true;
+		}
+		return false;
+	}
+
+	static boolean cCheck() {
+		for (int j = 0; j < N; j++) {
+			String s = "";
+			for (int i = 0; i < N; i++) {
+				s += map[i][j];
+			}
+			if (s.contains("ooooo"))
+				return true;
+		}
+		return false;
+	}
+
+	static boolean diaCheck() {
+		int cnt = 0;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (map[i][j] != 'o')
+					continue;
+				int a = i;
+				int b = j;
+				for (int k = 0; k < 5; k++) {
+					if (map[a][b] == 'o')
+						cnt++;
+					else
+						break;
+					if (a + 1 < N && b - 1 >= 0) {
+						a++;
+						b--;
+					} else
+						break;
+				}
+				if (cnt == 5)
+					return true;
+				else
+					cnt = 0;
+				a = i;
+				b = j;
+				for (int k = 0; k < 5; k++) {
+					if (map[a][b] == 'o')
+						cnt++;
+					else
+						break;
+					if (a + 1 < N && b + 1 < N) {
+						a++;
+						b++;
+					} else
+						break;
+				}
+				if (cnt == 5)
+					return true;
+				else
+					cnt = 0;
+			}
+		}
+		return false;
+	}
+}
