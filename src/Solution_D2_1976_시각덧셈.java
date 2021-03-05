f1a52781ad40520ca07536cefac0b9d6a4fca0dc
ByungBok-Jeon <ttlg789@gmail.com>
2021-03-05 10:24:50 +0900

    2021.03.05

--- src/Solution_D2_1976_시각덧셈.java
+++ src/Solution_D2_1976_시각덧셈.java
@@ -1,3 +1,8 @@
+
+/* D2.1976. 시각덧셈
+ * 출처 : https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=2&contestProbId=AV5PttaaAZIDFAUq&categoryId=AV5PttaaAZIDFAUq&categoryType=CODE&problemTitle=&orderBy=FIRST_REG_DATETIME&selectCodeLang=ALL&select-1=2&pageSize=10&pageIndex=2
+ * SW Expert Acadamy 문제
+ */
 import java.util.Scanner;
  
 public class Solution_D2_1976_시각덧셈 {
