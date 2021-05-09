c4c7bb82b9a4fe14712fbf3f8dc457b929a72954
ByungBok-Jeon <ttlg789@gmail.com>
2021-05-09 10:22:08 +0900

    2021.05.09

---
+++ src/Solution_D3_5356_의석이의세로로말해요.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Solution_D3_5356_의석이의세로로말해요 {
+	static int T;
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		T = scan.nextInt();
+		for (int t = 1; t <= T; t++) {
+			
+		}
+		
+		scan.close();
+	}
+
+}
